c22cb547327b8d5b13ac921747b77dc4aa03a20d
josephjacksonw <josephjacksonw@gmail.com>
2022-09-30 14:55:27 -0400

    all 7 order tests good fails

--- Vendor.Tests/ModelTests/DistributorTests.cs
+++ Vendor.Tests/ModelTests/DistributorTests.cs
@@ -1,72 +1,72 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Vendor.Models;
-using System.Collections.Generic;
-using System;
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using Vendor.Models;
+// using System.Collections.Generic;
+// using System;
 
-namespace Vendor.Tests
-{
-  [TestClass]
-  public class DistributorTests : IDisposable
-  {
+// namespace Vendor.Tests
+// {
+//   [TestClass]
+//   public class DistributorTests : IDisposable
+//   {
 
-    public void Dispose()
-    {
-      Distributor.ClearAll();
-    }
+//     public void Dispose()
+//     {
+//       Distributor.ClearAll();
+//     }
 
-    [TestMethod]
-    public void DistributorConstructor_CreatesInstanceOfDistributor_Distributor()
-    {
-      Distributor newDistributor = new Distributor("test distributor");
-      Assert.AreEqual(typeof(Distributor), newDistributor.GetType());
-    }
+//     [TestMethod]
+//     public void DistributorConstructor_CreatesInstanceOfDistributor_Distributor()
+//     {
+//       Distributor newDistributor = new Distributor("test distributor");
+//       Assert.AreEqual(typeof(Distributor), newDistributor.GetType());
+//     }
 
-    [TestMethod]
-    public void GetName_ReturnsName_String()
-    {
-      //Arrange
-      string name = "Test Distributor";
-      Distributor newDistributor = new Distributor(name);
+//     [TestMethod]
+//     public void GetName_ReturnsName_String()
+//     {
+//       //Arrange
+//       string name = "Test Distributor";
+//       Distributor newDistributor = new Distributor(name);
 
-      //Act
-      string result = newDistributor.Name;
+//       //Act
+//       string result = newDistributor.Name;
 
-      //Assert
-      Assert.AreEqual(name, result);
-    }
+//       //Assert
+//       Assert.AreEqual(name, result);
+//     }
 
-    [TestMethod]
-    public void GetId_ReturnsDistributorId_Int()
-    {
-      //Arrange
-      string name = "Test Distributor";
-      Distributor newDistributor = new Distributor(name);
+//     [TestMethod]
+//     public void GetId_ReturnsDistributorId_Int()
+//     {
+//       //Arrange
+//       string name = "Test Distributor";
+//       Distributor newDistributor = new Distributor(name);
 
-      //Act
-      int result = newDistributor.Id;
+//       //Act
+//       int result = newDistributor.Id;
 
-      //Assert
-      Assert.AreEqual(1, result);
-    }
+//       //Assert
+//       Assert.AreEqual(1, result);
+//     }
 
-    // [TestMethod]
-    // public void AddAlbum_AssociatesAlbumWithDistributor_AlbumList()
-    // {
-    //   //Arrange
-    //   string description = "Walk the dog.";
-    //   Album newAlbum = new Album(description);
-    //   List<Album> newList = new List<Album> { newAlbum };
-    //   string name = "Work";
-    //   Distributor newDistributor = new Distributor(name);
-    //   newDistributor.AddAlbum(newAlbum);
+//     // [TestMethod]
+//     // public void AddAlbum_AssociatesAlbumWithDistributor_AlbumList()
+//     // {
+//     //   //Arrange
+//     //   string description = "Walk the dog.";
+//     //   Album newAlbum = new Album(description);
+//     //   List<Album> newList = new List<Album> { newAlbum };
+//     //   string name = "Work";
+//     //   Distributor newDistributor = new Distributor(name);
+//     //   newDistributor.AddAlbum(newAlbum);
 
-    //   //Act
-    //   List<Album> result = newDistributor.Albums;
+//     //   //Act
+//     //   List<Album> result = newDistributor.Albums;
 
-    //   //Assert
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
+//     //   //Assert
+//     //   CollectionAssert.AreEqual(newList, result);
+//     // }
 
 
-  }
-}
+//   }
+// }
---
+++ Vendor.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,123 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Vendor.Models;
+
+namespace Vendor.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(String), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      // Arrange
+      string description = "Walk the dog.";
+      string description1 = "Malk the dog.";
+      Order newOrder = new Order(description);
+      // Act
+      string result = newOrder.Description;
+      // Assert
+      Assert.AreEqual(description1, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Order newOrder = new Order(description);
+      //Act
+      string updatedDescription = "Do the dishes";
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
+      //Assert
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      // Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
+      
+      // Act
+      List<Order> result = Order.GetAll();
+      List<Order> newList = new List<Order> { newOrder1 };
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
+      List<Order> newList = new List<Order> { newOrder1 };
+      //Act
+      List<Order> result = Order.GetAll();
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+    [TestMethod]
+    public void GetId_OrdersInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      //Arrange
+      string description = "walk the dog.";
+      Order newOrder = new Order(description);
+      //Act
+      int result = newOrder.Id;
+      //Assert
+      Assert.AreEqual(2, result);
+    }
+      [TestMethod]
+    public void Find_ReturnsCorrectOrder_Order()
+    {
+      //Arrange
+      string description01 = "walk the dog.";
+      string description02 = "wash the dishes";
+      Order newOrder = new Order(description01);
+      Order newOrder2 = new Order(description02);
+      //Act
+      Order result = Order.Find(1);
+      //Assert
+      Assert.AreEqual(newOrder2, result);
+    }
+
+    // [TestMethod]
+    // public void Find_ReturnsCorrectArtist_Artist()
+    // {
+    //   //Arrange
+    //   string name01 = "Work";
+    //   string name02 = "School";
+    //   Artist newArtist1 = new Artist(name01);
+    //   Artist newArtist2 = new Artist(name02);
+
+    //   //Act
+    //   Artist result = Artist.Find(2);
+
+    //   //Assert
+    //   Assert.AreEqual(newArtist2, result);
+    // }
+
+  }
+}
